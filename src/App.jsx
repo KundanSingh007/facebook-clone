ab0815d3d2b533a0d6f76d2c964fb90ba536e9f3
Kundan Singh <35429799+KundanSingh007@users.noreply.github.com>
2021-08-02 20:05:31 +0530

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Facebook</h1>
+
+      {/**Header */}
+
+      {/**App body */}
+
+      {/**Sidebar */}
+
+      {/**Feed */}
+ 
+      {/**widgets */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.jsx
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCFTEZO-I65dVo0AOuc9FxZa83I3vhK8R8",
+  authDomain: "facebook-clone-bykd.firebaseapp.com",
+  projectId: "facebook-clone-bykd",
+  storageBucket: "facebook-clone-bykd.appspot.com",
+  messagingSenderId: "320236998153",
+  appId: "1:320236998153:web:0032742d699aae7b50f2d8",
+};
