2e23c5e9e4dae4e509a4ece3a668dc0a974e7638
Kundan Singh <35429799+KundanSingh007@users.noreply.github.com>
2021-08-06 00:22:04 +0530

    post-finished

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -8,10 +8,22 @@ function Feed() {
   return (
     <div className="feed">
       <StoryReel />
-
       <MessageSender />
 
-      <Post />
+      <Post 
+     
+      profilePic="https://avatars.githubusercontent.com/u/35429799?v=4"
+      message = 'wow this works'
+      timestamp="This is a timestamp"
+      username="kundan singh"
+      image="https://yt3.ggpht.com/ytc/AKedOLRn7y2EFXPcZB4Bndc0X38tS81O0O0v8iRgD0iu=s900-c-k-c0x00ffffff-no-rj"  />
+      <Post 
+     
+      profilePic="https://avatars.githubusercontent.com/u/35429799?v=4"
+      message = 'wow this works'
+      timestamp="This is a timestamp"
+      username="kundan singh"
+      image="https://yt3.ggpht.com/ytc/AKedOLRn7y2EFXPcZB4Bndc0X38tS81O0O0v8iRgD0iu=s900-c-k-c0x00ffffff-no-rj"  />
       <Post />
       <Post />
       <Post />
--- src/Components/Feed/Post/Post.jsx
+++ src/Components/Feed/Post/Post.jsx
@@ -1,19 +1,53 @@
 import React from "react";
-import './Post.css'
-import { Avatar } from '@material-ui/core';
+import "./Post.css";
+import ThumbUpIcon from "@material-ui/icons/ThumbUp";
+import { Avatar } from "@material-ui/core";
+import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import ExpandMoreOutlinedIcon from "@material-ui/icons/ExpandMoreOutlined";
+import NearMeIcon from "@material-ui/icons/NearMe";
 
 function Post({ profilePic, image, username, timestamp, message }) {
-  return <div class="post">
+  return (
+    <div class="post">
       <div className="post__top">
-          <Avatar src={profilePic}
-          className="post__avatar" />
-          <div className="postInfo">
-              <h3>{username}</h3>
-              <p>Timestamp...</p>
-          </div>
-
+        <Avatar src={profilePic} className="post__avatar" />
+        <div className="post__topInfo">
+          <h3>{username}</h3>
+          <p>Timestamp...</p>
+        </div>
       </div>
-  </div>;
+
+      <div className="post__bottom">
+        <p>{message}</p>
+      </div>
+      <div className="post__image">
+        <img src={image} alt="" />
+      </div>
+
+      <div className="post__options">
+        <div className="post__option">
+          <ThumbUpIcon />
+          <p>Like</p>
+        </div>
+
+        <div className="post__option">
+          <ChatBubbleOutlineIcon />
+          <p>Comment</p>
+        </div>
+
+        <div className="post__option">
+          <NearMeIcon />
+          <p>Share</p>
+        </div>
+
+        <div className="post__option">
+          <AccountCircleIcon />
+          <ExpandMoreOutlinedIcon />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Post;
