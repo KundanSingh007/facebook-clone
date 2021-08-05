4469bfc27c0ef852c98d3229161357ba5a09aa69
Kundan Singh <35429799+KundanSingh007@users.noreply.github.com>
2021-08-05 21:34:54 +0530

    post-created

--- src/Components/Feed/Post/Post.jsx
+++ src/Components/Feed/Post/Post.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React from "react";
+import './Post.css'
+import { Avatar } from '@material-ui/core';
 
-function Post() {
-    return (
-        <div class="post"> 
-            
-            
-        </div>
-    )
+function Post({ profilePic, image, username, timestamp, message }) {
+  return <div class="post">
+      <div className="post__top">
+          <Avatar src={profilePic}
+          className="post__avatar" />
+          <div className="postInfo">
+              <h3>{username}</h3>
+              <p>Timestamp...</p>
+          </div>
+
+      </div>
+  </div>;
 }
 
-export default Post
+export default Post;
